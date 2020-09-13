8c6af3a938181f99e81642e9b816144b77c84679
Snowing222 <snowing222@gmail.com>
2020-09-13 18:43:17 -0400

    basic function works

--- app/controllers/playdates_controller.rb
+++ app/controllers/playdates_controller.rb
@@ -40,6 +40,39 @@ class PlaydatesController < ApplicationController
             redirect '/login'
         end
     end
+
+    get '/playdates/:id/edit' do
+        if_not_logged_in_redirect_to_index
+        @playdate = Playdate.find(params[:id])
+        if current_user.id == session[:user_id] 
+            if @playdate
+                erb :'playdates/edit_playdate'
+            else
+                erb :'/playdates/failure'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/playdates/:id' do
+        playdate = Playdate.find_by(id: params[:id])
+        playdate.title = params['title']
+        playdate.date = params['date']
+        playdate.location = params['location']
+        playdate.zipcode = params['zipcode']
+        playdate.description = params['description']
+        playdate.baby_ids = params['babies']
+        redirect "/playdates/#{playdate.id}"
+          
+    end
+
+    delete '/playdates/:id' do
+        playdate = Playdate.find_by(id: params[:id])
+        playdate.destroy
+        redirect '/playdates'
+
+    end
   
   
 end
