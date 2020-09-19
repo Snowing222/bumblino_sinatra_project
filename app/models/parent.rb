8b7a0075f00df205e555297d206f87aed9ec308a
Snowing222 <snowing222@gmail.com>
2020-09-18 20:59:28 -0400

    rename created_playdates

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,8 +2,8 @@ class Parent < ActiveRecord::Base
     has_secure_password
     validates :name, :email, presence: true, uniqueness: true
     has_many :babies, dependent: :delete_all
-    has_many :owned_playdates, :class_name => "Playdate", :foreign_key => "creator_id", dependent: :delete_all
-    
+    has_many :created_playdates, :class_name => "Playdate", :foreign_key => "creator_id", dependent: :delete_all
+  
 
     def slug 
         self.name.gsub(" ","-")
